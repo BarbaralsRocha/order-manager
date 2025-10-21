09c9fd2df85a8ebaa58532437784c354ec5d81cd
Barbara Rocha <barbaraluizasr@gmail.com>
2025-10-20 19:48:40 -0500

    feat: test route

--- src/frontend/features/Products/redux/Products.api.ts
+++ src/frontend/features/Products/redux/Products.api.ts
@@ -37,7 +37,7 @@ export const ProductsApi = createApi({
       { productId: number }
     >({
       query: ({ productId }) => ({
-        url: `${endpoint}/product/${productId}`,
+        url: `${endpoint}/products/${productId}`,
         method: 'DELETE',
       }),
     }),
