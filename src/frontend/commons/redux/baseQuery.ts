e68e38483e15efeb2ac000748a6377d4d6d53e71
Barbara Rocha <barbaraluizasr@gmail.com>
2025-10-20 19:43:42 -0500

    clean console

--- src/frontend/commons/redux/baseQuery.ts
+++ src/frontend/commons/redux/baseQuery.ts
@@ -2,10 +2,10 @@ import { fetchBaseQuery } from '@reduxjs/toolkit/query';
 import { RootState } from './store';
 
 export const baseQuery = fetchBaseQuery({
+  // baseUrl: process.env.REACT_APP_API_URL,
   baseUrl: process.env.REACT_APP_BASE_URL,
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
-    console.log({ token });
     headers.set('Authorization', `Bearer ${token}`);
     headers.set('Content-Type', 'application/json');
     return headers;
